1357aecf62e7d2386b2d303bec89693527a3ef87
Chris1112220 <Christopher.roberts11220@gmail.com>
2025-11-21 23:00:15 -0500

    Add sample CSV and update main auto-detect logic

--- src/main.py
+++ src/main.py
@@ -1,5 +1,19 @@
+import os
 from importer.pnc_importer import import_pnc_csv
 
+DATA_DIR = "data"
+
+
+def get_any_csv():
+
+    for filename in os.listdir(DATA_DIR):
+        if filename.lower().endswith(".csv"):
+            return os.path.join(DATA_DIR, filename)
+
+    raise FileNotFoundError("No CSV files found in the data/ ")
+
+
 if __name__ == "__main__":
-    csv_path = "data/accountActivityExport.csv"
+    csv_path = get_any_csv()
+    print(f"Using CSV file: {csv_path}")
     import_pnc_csv(csv_path)
